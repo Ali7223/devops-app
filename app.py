bfcbdcea6f81f99bdcba36af98e25e20a45cbc2d
Ali Saad <saadali575@gmail.com>
2026-05-11 18:12:22 +0200

    Clean CI/CD setuup

---
+++ app.py
@@ -0,0 +1,12 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        message = os.getenv("MESSAGE", "Default Message")
+        self.wfile.write(message.encode())
+
+HTTPServer(('0.0.0.0', 8080), Handler).serve_forever()
